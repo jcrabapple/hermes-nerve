f69875501560f2be90a59d1ce7fd7aaaccec330c
KeelTrace <218190424+keeltrace@users.noreply.github.com>
2026-09-18 00:15:54 -0700

    feat: export context engine and version

--- hermes_jev/__init__.py
+++ hermes_jev/__init__.py
@@ -2,5 +2,7 @@
 
 from .client import JevClient, JevError, JevResponse
 from .engine import DecisionEngine, DecisionResult
+from .context_engine import JevContextEngine
+from .provenance import VERSION
 
-__all__ = ["DecisionEngine", "DecisionResult", "JevClient", "JevError", "JevResponse"]
+__all__ = ["DecisionEngine", "DecisionResult", "JevClient", "JevError", "JevResponse", "JevContextEngine", "VERSION"]
